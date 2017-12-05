6dd068b4ca4704dac55c61a45a6e6a428e084655
Robin Broda <robin@broda.me>
2017-12-05 16:35:37 +0100

    Add solution for day 5 (part 2)

---
+++ solution-5-2.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+input = File
+  .readlines("input-5.txt")
+  .map(&:to_i)
+
+i, output = 0, 0
+
+while i < input.length && i >= 0 do
+  step = input[i]
+  input[i] < 3 ? input[i] += 1 : input[i] -= 1
+  i += step 
+  output += 1
+end
+
+puts output
