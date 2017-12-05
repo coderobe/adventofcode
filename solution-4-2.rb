ae3f1a5632b3ac1ed28ee7b64688ddca08363349
Robin Broda <robin@broda.me>
2017-12-05 04:59:38 +0100

    Add solution for day 4 (part 2)

---
+++ solution-4-2.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+output = File
+  .readlines("input-4.txt")
+  .map do |e|
+    e
+      .split
+      .map{|v| v.chars.sort}
+      .group_by(&:itself)
+      .map{|k, v| [k, v.count]}
+      .select{|v| v.last > 1}
+      .length > 0 ? 0 : 1
+  end
+  .reduce(&:+)
+
+puts output
