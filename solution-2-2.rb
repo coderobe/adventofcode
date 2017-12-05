621ac634e7608e82fa002df2ee662379d2c92ac5
Robin Broda <robin@broda.me>
2017-12-05 04:34:40 +0100

    Clean up solution for day 2 (part 2)

--- solution-2-2.rb
+++ solution-2-2.rb
@@ -2,8 +2,8 @@
 
 output = File
   .readlines("input-2.txt")
-  .map{|e| e.split.map(&:to_i)}
-  .map{|e| e.each_with_index.map{|v, i| e.each_with_index.map{|v2, t| (t != i) ? v.to_f/v2 : 0}}.flatten.select{|v| v % 1 == 0}.reduce(&:+).to_i}
+  .map{|e| e.split.map(&:to_f)}
+  .map{|e| e.each_with_index.map{|v, i| e.each_with_index.map{|v2, t| (t != i) ? v/v2 : 0}}.flatten.select{|v| v % 1 == 0}.reduce(&:+).to_i}
   .reduce(&:+)
 
 puts output
