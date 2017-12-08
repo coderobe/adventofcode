3f5d96cf0fb5b96ff6295f71bd00cd0aa7186047
Robin Broda <robin@broda.me>
2017-12-08 13:38:07 +0100

    Remove unnecessary leftovers

--- solution-8.rb
+++ solution-8.rb
@@ -4,8 +4,6 @@ input = File
   .readlines("input-8.txt")
   .map(&:split)
   .each{|e| e[1] == "dec" ? e[1] = "-=" : e[1] = "+="}
-  .each{|e| e[2] = e[2].to_i}
-  .each{|e| e[6] = e[6].to_i}
   .map{|e| { :register => e[0], :operator => e[1], :argument => e[2], :branch_by => e[3], :comparators => [e[4], e[6]], :condition => e[5] }}
 
 registers = input
